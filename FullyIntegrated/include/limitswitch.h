1232899e9f20b2e1d2bf2934e71c86c97a918c18
Sean <swkonz@gmail.com>
2019-02-28 03:43:39 -0800

    Added limit switch testing code

--- FullyIntegrated/include/limitswitch.h
+++ FullyIntegrated/include/limitswitch.h
@@ -3,7 +3,7 @@
 #define FRONT_LIMIT_PIN 9
 #define RIGHT_LIMIT_PIN 6
 #define LEFT_LIMIT_PIN 8
-#define BACK_LIMIT_PIN 6
+#define BACK_LIMIT_PIN 7
 
 void limit_switch_setup() {
     pinMode(FRONT_LIMIT_PIN, INPUT_PULLUP);
@@ -26,4 +26,20 @@ bool left_limit_pressed() {
 
 bool back_limit_pressed() {
     return digitalRead(BACK_LIMIT_PIN) == LOW;
+}
+
+void limit_switch_test() {
+    if (front_limit_pressed()) {
+        Serial.println("Front LS pressed");
+    }
+    if (right_limit_pressed()) {
+        Serial.println("Right LS pressed");
+    }
+    if (left_limit_pressed()) {
+        Serial.println("Left LS pressed");
+    }
+    if (back_limit_pressed()) {
+        Serial.println("Back LS pressed");
+    }
+    delay(100);
 }

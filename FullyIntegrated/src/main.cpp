c9368046238b24f2af5b4ba7e26688491fdf7c2c
Jacob Meisel <47841665+jacobmeisel@users.noreply.github.com>
2019-02-28 17:41:04 -0800

    Added simple wall bouncing test

--- FullyIntegrated/src/main.cpp
+++ FullyIntegrated/src/main.cpp
@@ -2,6 +2,27 @@
 #include "motor.h"
 #include "limitswitch.h"
 
+
+/* Put this function in loop.
+ * This function is a proof-of-concept of using limit switches to wall bounce.
+ * If this fails, we might need to implement line following.
+ * The car will travel forwards bouncing along the LEFT wall, until it hits something
+ * in front, at which point it will stop moving indefinitely.
+ */
+void simple_wall_bouncing_test() {
+  if (left_limit_pressed()) {
+    set_L_wheel(255);
+    set_R_wheel(-200);
+  } else {
+    set_L_wheel(170);
+    set_R_wheel(255);
+  }
+  if(front_limit_pressed()) {
+    stop_wheels();
+    while(true);
+  }
+}
+
 void setup() {
   Serial.begin(9600);
   motor_setup();
@@ -9,8 +30,7 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-    // motor test code with input from computer
+  // motor test code with input from computer
   /*if(Serial.available()) {
     int val = Serial.parseInt(); // try typing numbers from -255 to 255 into serial monitor
     Serial.print("You said: ");
@@ -19,5 +39,8 @@ void loop() {
     set_L_wheel(val);
     set_R_wheel(val);
   }*/
-  limit_switch_test();
+
+  //limit_switch_test();
+
+  simple_wall_bouncing_test();
 }
